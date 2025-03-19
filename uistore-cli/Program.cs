510467a26637eb24859900d29cc709362f2abafc
copperminesp <son5121502@gmail.com>
2025-03-20 05:14:25 +0900

    chore: change directories

--- uistore-cli/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace uistore_cli;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
--- uistore-lib/Exceptions.cs
+++
@@ -1,3 +0,0 @@
-namespace CloudInteractive.UniFiStore;
-
-public class BuildIdException(string message) : Exception(message);
