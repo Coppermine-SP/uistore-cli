1349831607991cca94da16ba72e8af4571998468
copperminesp <son5121502@gmail.com>
2025-03-21 03:00:26 +0900

    feat: ToString(), internal constructor

--- src/uistore-lib/Product.cs
+++ src/uistore-lib/Product.cs
@@ -3,19 +3,35 @@
  *  Copyright (C) 2025 Coppermine-SP
  */
 using System.Collections.ObjectModel;
+using System.Security.Claims;
+using System.Text;
 
 namespace CloudInteractive.UniFiStore;
 
-public struct Product()
+public class Product
 {
+    internal Product()
+    {
+    }
+
     public string Id { get; internal set; }
+    public string Name { get; internal set; }
     public string Title { get; internal set; }
     public string Category { get; internal set; }
     public string Description { get; internal set; }
     public string ThumbnailUrl { get; internal set; }
-    
+
     internal List<Variant> VariantsList = new List<Variant>();
     public ReadOnlyCollection<Variant> Variants => VariantsList.AsReadOnly();
-    
 
+    public override string ToString()
+    {
+        StringBuilder builder = new StringBuilder($"{Title}\n");
+        foreach (Variant v in VariantsList)
+            builder.Append(
+                $"   - {v.Sku}, Avaliable:{v.Quantity}, Price:{(v.Amount / 100.0).ToString("F2")}{v.Currency}, HasUICare:{v.HasUiCare.ToString()}\n");
+        builder.Append("\n");
+
+        return builder.ToString();
+    }
 }
