69bef730250b8123888db4e0d96af9665f669b54
copperminesp <son5121502@gmail.com>
2025-03-21 03:00:58 +0900

    feat: internal constructor

--- src/uistore-lib/Variant.cs
+++ src/uistore-lib/Variant.cs
@@ -4,12 +4,14 @@
  */
 namespace CloudInteractive.UniFiStore;
 
-public struct Variant
+public class Variant
 {
+    internal Variant() {}
     public string Id { get; internal set; }
     public string Sku { get; internal set; }
-    public int Amount { get; internal set; }
+    public uint Amount { get; internal set; }
     public string Currency { get; internal set; }
     public bool HasUiCare { get; internal set; }
-    public int Quantity { get; internal set; }
+    
+    public uint Quantity { get; internal set; }
 }
